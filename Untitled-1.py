08feae94064669881dce81d02685682854515fd7
madhav pandit <madhavdisar07@gmail.com>
2025-08-21 08:51:48 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,4 @@
+n=int(input("entre a natural number:"))
+for n in range(0,n):
+    n=n+1
+    print(n)
---
+++ Untitled-5.py
@@ -0,0 +1,5 @@
+# Take input from the user
+N = int(input("Enter a natural number N: "))
+for i in range(1, N + 1):
+        total=+N
+print("The sum of all natural numbers from 1 to", N, "is:", total)
